c9bd447758673b63d1cd2c9a4718a744e63af8c7
Werehog2006 <gerardobarnett@hotmail.com>
2021-10-21 14:24:21 -0700

    OOP Exercises regarding abstraction and exception

---
+++ Week2/oop-demo/src/main/java/com/revature/interfaces/Doable.java
@@ -0,0 +1,13 @@
+package com.revature.interfaces;
+
+public interface Doable extends DoesNothingable, Cloneable {
+
+	// public static final int napLength = 8;
+	int napLength = 8;
+	
+	void doSomething();
+	
+	default void doSomethingElse() {
+		System.out.println("Default implementation");
+	}
+}
