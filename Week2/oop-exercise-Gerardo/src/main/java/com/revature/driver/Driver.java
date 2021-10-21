5caa6bc19fb8dae5cbcb96abd7b4d534433d0afc
Werehog2006 <gerardobarnett@hotmail.com>
2021-10-21 14:59:42 -0700

    OOP Exercise Gerardo

---
+++ Week2/oop-exercise-Gerardo/src/main/java/com/revature/driver/Driver.java
@@ -0,0 +1,26 @@
+package com.revature.driver;
+
+
+import java.util.Scanner;
+
+import com.revature.services.OOPservice;
+
+public class Driver {
+//encapsulation, inheritance, polymorphism, abstraction
+	
+	private static Scanner val = new Scanner(System.in);
+	public static void main(String[] args) {
+		String value = val.nextLine();;
+		
+		OOPservice oop = new OOPservice();
+		switch(value) {
+		
+		
+		}
+		val.close();
+		
+		
+		
+	}
+	
+}
