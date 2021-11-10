96960d635461fbf8a28b3891c1fab9ad49f50206
Werehog2006 <gerardobarnett@hotmail.com>
2021-11-09 21:53:19 -0700

    Project0 almost done

--- Week3/Project0/src/test/java/com/revature/CustomerDaoTest.java
+++ Week3/Project0/src/test/java/com/revature/CustomerDaoTest.java
@@ -1,43 +1,47 @@
 package com.revature;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.revature.models.Customer;
-import com.revature.repositories.CustomerList;
 import com.revature.repositories.CustomerDao;
+import com.revature.repositories.CustomerList;
+import com.revature.repositories.CustomerPostgres;
 
 public class CustomerDaoTest {
 	
     
-//    private CustomerDao cd = new CustomerList();
-//
-//    @BeforeEach
-//    public void setUp() {
-//    	cd = new CustomerList();
-//    }
-//
+    private CustomerDao cd = new CustomerList();
+
+    @BeforeEach
+    public void setUp() {
+    	cd = new CustomerPostgres();
+    }
+
 //    @Test
 //    public void getAllCustomers() {
 //    	List<Customer> expected = new ArrayList<>();
-//    	expected.add(new Customer(0, "Gerardo", "GerardoB", "Opeth"));
+//    	expected.add(new Customer(0, "Gerardo", "GerardoB", "Opeth", 12345678));
 //
-//    	assertEquals(expected, cd.getAll());
+//    	assertEquals(expected, cd.getCustomers());
 //    }
 //
 //    @Test
 //    public void getCustomerByIdExist() {
-//    	Customer expected = new Customer(0, "Gerardo", "GerardoB", "Opeth");
+//    	Customer expected = new Customer(0, "Gerardo", "GerardoB", "Opeth", 12345678);
 //
-//    	assertEquals(expected, cd.getById(0));
+//    	assertEquals(expected, cd.getCustomerById(0));
 //    }
+    
+    @Test
+	public void getByIdValid() {
+		Customer expected = new Customer(1, "Gerardo", "GerardoB", "Opeth", 12345678);
+		Customer actual = cd.getCustomerById(1);
+		assertEquals(expected, actual);
+	}
+    
 //
 //    @Test
 //    public void getCustomerByIdDoesNotExist() {
