7df9a7538e91c765685f255f5b3c532751b70b53
Werehog2006 <gerardobarnett@hotmail.com>
2021-11-19 14:09:07 -0700

    Still trying to figure out some stuff before advancing with the project

---
+++ Week6/Project1/src/main/java/com/revature/repositories/UsersDao.java
@@ -0,0 +1,12 @@
+package com.revature.repositories;
+
+import com.revature.models.User;
+
+public interface UsersDao {
+	
+	User getUserByUsername(String username);
+	User getUserById(int userId);
+	User viewMyInfo(User u);
+	User updateMyInfo(User u);
+
+}
